15d28d58632cea61e2631c5bcc88186d6c7050e4
David B. Dahl <david@ddahl.org>
2017-05-19 15:44:06 -0600

    Progress

--- examples/test.R
+++ examples/test.R
@@ -13,16 +13,75 @@ a$toAdjacencyMatrix()
 
 library(rscala)
 s <- austin:::s
+
 ggp <- s$.org.ddahl.austin.GeneralizedGammaProcess$apply(1.0,1.0,0.0)
 
-ggp$levyIntensity(500.0,1000.0)
-ggp$levyIntensity(100.0,500.0)
-ggp$levyIntensity(10.0,100.0)
-ggp$levyIntensity(10.0,100.0)
+ggp$intensityIntervalArea(10.0,10000.0)
+ggp$intensityTailArea(0.5)
+
+ggp$intensityTailArea(0.000000001)
+ggp$intensityTailArea(0.000000001^(1/2))
+ggp$intensityTailArea(0.000000001^(1/4))
+ggp$intensityTailArea(0.000000001^(1/8))
+ggp$intensityTailArea(0.000000001^(1/16))
+ggp$intensityTailArea(0.000000001^(1/32))
+
+ggp$intensityTailArea(0.0000001)
+ggp$intensityTailArea(0.0000001^(1/2))
+ggp$intensity(0.0000001)
+ggp$intensity(0.0000001^(1/2))
+ggp$intensity(0.0000001^(1/3))
+
+
+
+
+
+ggp$steps(20,0.0001, 0.01)
+
+f <- function(t,m,x,kappa,gamma) {
+  exp(-kappa*x) - m*exp(-kappa*t*x)/t^(1+gamma)
+}
+
+m <- 10
+x <- 0.00001
+while ( x < 10 ) {
+  cat("---\n")
+  cat("# ",x,"\n")
+  print(ggp$intensity(x))
+  u <- uniroot(function(t) f(t,m,x,2,0.5),c(1,20))
+  print(u$root)
+  print(ggp$intensityIntervalArea(x,x*u$root))
+  x <- x*u$root
+  print(m*ggp$intensity(x))
+}
+
+
+
+
+ggp$levyIntensityIntervalSlow(10.0,10000.0,100000000L)
+ggp$levyIntensityTail(10.0)
+
+rpois(1,ggp$levyIntensity(500.0,1000.0))
+
+dpois(0,ggp$levyIntensity(500.0,1000.0))
+dpois(0,ggp$levyIntensity(100.0,500.0))
+dpois(0,ggp$levyIntensity(10.0,100.0))
+dpois(0,ggp$levyIntensity(3.0,10.0))
 
 setmaxEval <- function(x=ggp,value=100L) s %!% '
   x.maxEval = value
 '
 
 setmaxEval(ggp,100000)
+
+
+kappa <- 1
+gamma <- 0
+p <- function(x) exp(-kappa*x) * x^(-1-gamma)
+curve(p,0,2)
+
+p(0.01)
+p(0.02)
+p(0.1)
+p(0.2)
 
--- src/main/scala/org/ddahl/austin/GeneralizedGammaProcess.scala
+++ src/main/scala/org/ddahl/austin/GeneralizedGammaProcess.scala
@@ -1,33 +1,78 @@
 package org.ddahl.austin
 
 import org.apache.commons.math3.util.FastMath.{exp, pow}
-import org.apache.commons.math3.special.Gamma.{gamma => Gamma}
+import org.apache.commons.math3.special.Gamma.{regularizedGammaQ, gamma => Gamma}
 import org.apache.commons.math3.analysis.UnivariateFunction
 import org.apache.commons.math3.analysis.integration.SimpsonIntegrator
+import org.apache.commons.math3.analysis.solvers.BrentSolver
+import org.apache.commons.math3.distribution.PoissonDistribution
+import wilx.math.exponential.integrals.ExponentialIntegrals.{Exponential_Integral_Ei => expint}
 
 case class GeneralizedGammaProcess(alpha: Double, kappa: Double, gamma: Double) {
 
-  if (alpha <= 0) new IllegalArgumentException("alpha must be greater than 0.")
-  if (kappa < 0) new IllegalArgumentException("kappa must be greater than or equal to 0.")
-  if ((gamma < 0) || (gamma >= 1)) new IllegalArgumentException("gamma must be greater than or equal to 0 and less than 1.")
+  // Constraints in Caron & Fox (2017).
+  require(alpha>0, "alpha must be greater than 0.")
+  require(kappa>=0, "kappa must be greater than or equal to 0.")
+  require(gamma<1, "gamma must be less than 1.")
+  if ( kappa == 0 ) require(gamma > 0, "gamma must be greater than 0 when kappa is 0.")
 
-  private lazy val integrator = new SimpsonIntegrator
+  // Extra constraint to make things interesting...
+  require(gamma>=0, "gamma must be greater than or equal to zero for there to be an infinite number of jumps.")
 
-  private val func = new UnivariateFunction {
-    def value(w: Double): Double = exp(-kappa*w) * pow(w,-1-gamma)
+  def intensity(x: Double): Double = exp(-kappa*x) * pow(x,-1-gamma)
+
+  def intensityTailArea(x: Double): Double = {
+    require(x>=0,"x must be greater than or equal to 0.")
+    alpha * ( kappa match {
+      case 0.0 =>
+        pow(x,-gamma) / ( Gamma(1-gamma) * gamma )
+      case _ =>
+        pow(kappa,gamma) * incompleteGamma(-gamma,kappa*x) / Gamma(1-gamma)
+    } )
   }
 
-  var maxEval = 1000
+  def intensityIntervalArea(a: Double, b: Double): Double = {
+    require(a<b, "a must be less than b.")
+    intensityTailArea(a) - intensityTailArea(b)
+  }
 
-  def levyIntensity(a: Double, b: Double): Double = {
-    val v = integrator.integrate(maxEval, func, a, b) * alpha / Gamma(1-gamma)
-    println(integrator.getEvaluations)
-    println(integrator.getIterations)
-    v
+  def steps(courseness: Double, smallestWeight: Double, epsilon: Double) = {
+    var x = smallestWeight
+    var mm = courseness
+    val solver = new BrentSolver()
+    val func = new UnivariateFunction {
+      override def value(t: Double): Double = exp(-kappa*x) - courseness*exp(-kappa*t*x)*pow(t,-1-gamma)
+    }
+    while ( intensityTailArea(x) > epsilon ) {
+      println("----")
+      println("x = "+x)
+      mm = solver.solve(100, func, 1.0, mm)
+      println("t = "+mm)
+      x = x * mm
+    }
+  }
+
+  def intensityIntervalAreaSlow(a: Double, b: Double, maxEval: Int): Double = {
+    require(a<b, "a must be less than b.")
+    val integrator = new SimpsonIntegrator
+    val func = new UnivariateFunction {
+      def value(x: Double): Double = intensity(x)
+    }
+    integrator.integrate(maxEval, func, a, b) * alpha / Gamma(1-gamma)
+  }
+
+  private def incompleteGamma(a: Double, x: Double): Double = {
+    if ( a > 0 ) {
+      Gamma(a) * regularizedGammaQ(a,x)
+    } else if ( a < 0 ) {
+      (-pow(x,a)*exp(-x)+incompleteGamma(a+1,x))/a
+    } else {
+      -expint(-x)
+    }
   }
 
   def sample(rbase: (Int) => Array[Double]): Unit = {
-
+    //val n = PoissonDistribution()
   }
 
 }
