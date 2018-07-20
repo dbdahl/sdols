973dd7daad3ec9692c5686de897a2769050d2a27
David B. Dahl <dahl@stat.byu.edu>
2018-07-20 16:18:36 -0600

    Improved.

--- R/sdols/R/onLoad.R
+++ R/sdols/R/onLoad.R
@@ -1,13 +1,16 @@
 #' @import commonsMath
 
 .onLoad <- function(libname, pkgname) {
-  assign.callback <- function(s) s + '
-    import org.ddahl.sdols._
-    import org.ddahl.sdols.network._
-    import org.ddahl.sdols.clustering._
-    import org.ddahl.sdols.featureallocation._
-    import org.apache.commons.math3.random.{ RandomDataGenerator => RDG }
-  '
+  assign.callback <- function(s) {
+    s + '
+      import org.ddahl.sdols._
+      import org.ddahl.sdols.network._
+      import org.ddahl.sdols.clustering._
+      import org.ddahl.sdols.featureallocation._
+      import org.apache.commons.math3.random.{ RandomDataGenerator => RDG }
+    '
+    scalaUnserializeRegister(scalaUnserialize.clusterings,s)
+  }
   scalaPackage(c(pkgname,"commonsMath"),assign.callback)
 }
 
--- R/sdols/R/scalaConvert.clustering.R
+++ R/sdols/R/scalaConvert.clustering.R
@@ -14,6 +14,60 @@
 #'
 #' @import rscala
 #' @export
+#' 
+scalaUnserialize.clusterings <- function(reference, type=scalaType(reference), bridge=scalaFindBridge(reference), verbose=FALSE, names=NULL, withParameters=TRUE) {
+  if ( verbose ) cat("scalaUnserialize.clusterings: Trying...\n")
+  if ( grepl("^List\\[org\\.ddahl\\.shallot\\.parameter\\.partition\\.Partition\\[.*\\]$",type) ) {
+    if ( verbose ) cat("scalaUnserialize.clusterings: Found match...\n")
+    if ( withParameters && ( type == "List[org.ddahl.shallot.parameter.partition.Partition[org.ddahl.rscala.RObject]]" ) ) {
+      if ( verbose ) cat("scalaUnserialize.clusterings: ... with parameters.\n")
+      r <- s(ref=reference) ^ '
+        val list = ref.map { partition =>
+          (partition.toLabelsWithParameters, partition.nClusters)
+        }
+        val labels = list.map(_._1._1).toArray
+        val parameters = list.map(_._1._2).flatten
+        val sizes = list.map(_._2).toArray
+        (labels, parameters, sizes)
+      '
+      labels <- r$"_1"()
+      parameters <- scalaUnserialize.generic(r$"_2"(),bridge=bridge,verbose=verbose)
+      sizes <- r$"_3"()
+      p <- vector(length(sizes), mode="list")
+      j <- 1
+      for ( i in seq_along(sizes) ) {
+        p[[i]] <- parameters[j:(j+sizes[i]-1)]
+        j <- j + sizes[i]
+      }
+      parameters <- p
+    } else {
+      if ( verbose ) cat("scalaUnserialize.clusterings: ... without parameters.\n")
+      labels <- s(ref=reference) * 'ref.map(_.toLabels).toArray'
+      parameters <- NULL
+    }
+    labels <- labels + 1L
+    colnames(labels) <- names
+    if ( verbose ) cat("scalaUnserialize.clusterings: Success.\n")
+    list(labels=labels, parameters=parameters)
+  } else stop("Unsupported type.")
+}
+
+#' \strong{(Developers Only:)} Convert Between R and Scala Representations of Clusterings
+#'
+#' \strong{This function is not intended for end users}, but is exported for the benefit of
+#' developers whose wish to write other packages that depend on this package.
+#'
+#' @param x Either: i. a list containing a elements 'labels' and 'parameters' which are,
+#'          respectively, a matrix of cluster labels and list whose number of elements
+#'          is the same as the number of rows of 'labels' and whose elements are are lists
+#'          whose length is the number of clusters for the corresponding cluster.
+#'          ii. a Scala reference to a clustering or a list of Scala references to
+#'          clusterings.
+#' @param names A character vector giving the item labels when converting from Scala to R.
+#' @param withParameters A logical indicating whether model parameters should also be converted.
+#'
+#' @import rscala
+#' @export
 
 scalaConvert.clustering <- function(x, names=NULL, withParameters=TRUE) {
   if ( inherits(x,"rscalaReference") ) {
