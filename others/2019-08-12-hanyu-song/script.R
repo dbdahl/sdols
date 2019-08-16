b44c92de29665b18d0307b5374c6fcedf96376cd
David B. Dahl <dahl@stat.byu.edu>
2019-08-16 15:10:07 -0600

    Yes

--- others/2019-08-12-hanyu-song/script.R
+++ others/2019-08-12-hanyu-song/script.R
@@ -1,15 +1,14 @@
 load("clusterings.RData")
 
-library(sdols)
+# library(sdols)
+# rscala::scalaMemory(sdols:::s)
+# system.time(psm <- expectedPairwiseAllocationMatrix(clusterings))
+# rscala::scalaMemory(sdols:::s)
+# system.time(e1 <- salso(psm, loss = "lowerBoundVariationOfInformation", nCandidates = 20, budgetInSeconds = 10))
+# rscala::scalaMemory(sdols:::s)
 
-rscala::scalaMemory(sdols:::s)
+library(rpm)
+system.time(psm <- psm(clusterings))
+system.time(e2 <- salso(psm, loss="vilb", nCandidates=1))
 
-system.time(psm <- expectedPairwiseAllocationMatrix(clusterings))
-system.time(psm <- rpm::psm(clusterings))
-
-rscala::scalaMemory(sdols:::s)
-
-system.time(salso(psm, loss = "lowerBoundVariationOfInformation", nCandidates = 20, budgetInSeconds = 10))
-
-rscala::scalaMemory(sdols:::s)
 
